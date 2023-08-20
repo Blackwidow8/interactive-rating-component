182feff4ac6922c0b2b6f601527e8cca5be936b8
Susan Wafula <125906521+Blackwidow8@users.noreply.github.com>
2023-08-20 15:14:09 +0300

    Add files via upload
    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const container=document.querySelector('.container')
+
+const thanksContainer= document.querySelector('.thank-you')
+const submitButton=document.getElementById('submit')
+const rating=document.getElementById('rating')
+const rates=document.querySelectorAll('#btn')
+
+submitButton.addEventListener('click', () =>{
+    thanksContainer.classList.remove('hidden')
+    container.style.display= 'none'     
+})
+  
+rates.forEach((rate) =>{
+    rate.addEventListener('click',()=>{
+       rating.innerHTML=rate.innerHTML
+    })
+})
